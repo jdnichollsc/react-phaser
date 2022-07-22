49d6124614fef525b18d4d711bc6f1cfc3ad101d
jdnichollsc <jdnichollsc@hotmail.com>
2022-07-22 11:32:33 -0500

    1 file modified

--- game/scenes/main.ts
+++ game/scenes/main.ts
@@ -1,21 +1,21 @@
-import { Scene, GameObjects, Cameras } from "phaser";
+import { Scene, GameObjects, Cameras } from 'phaser';
 
 export class MainScene extends Scene {
   private helloLabel!: GameObjects.Text;
   private camera!: Cameras.Scene2D.Camera;
   init() {
     this.camera = this.cameras.main;
-    this.camera.setBackgroundColor("#24252A");
+    this.camera.setBackgroundColor('#24252A');
   }
 
   create() {
     const { centerX, centerY } = this.camera;
     this.helloLabel = this.add
-      .text(centerX, centerY, "Hello World", {
-        fontFamily: "Rancho",
-        fontSize: 40
+      .text(centerX, centerY, 'Hello World', {
+        fontFamily: 'Rancho',
+        fontSize: 80,
       })
-      .setShadow(5, 5, "#5588EE", 0, true, true)
+      .setShadow(5, 5, '#5588EE', 0, true, true)
       .setOrigin(0.5, 0.5);
   }
 
